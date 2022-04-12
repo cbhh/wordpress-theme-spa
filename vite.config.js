cd00da87d7ab1a1dd2253ef348e846dc59898cb0
zxcvb <xuejingwei_1129@outlook.com>
2022-04-12 20:56:35 +0800

    [自定义app配置]+[自定义配置写入index.html]

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,66 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import externalGlobals from "rollup-plugin-external-globals";
+import { createHtmlPlugin } from "vite-plugin-html";
 import path from "path";
+import wpAppConfig from "./wp-app.config";
 
+/**
+ * 生成站点元信息
+ * @returns meta标签字符串
+ */
+function generateSiteMeta() {
+	var metaArray = [];
+	if (wpAppConfig.siteDescription) {
+		metaArray.push(`<meta name="description" content="${wpAppConfig.siteDescription}"/>`);
+	}
+	return metaArray.join("");
+}
+/**
+ * 生成站点link标签
+ * @returns link标签字符串
+ */
+function generateSiteLink() {
+	var linkArray = [];
+	if (wpAppConfig.siteIcon) {
+		linkArray.push(
+			`<link rel="icon" href="${wpAppConfig.siteIcon}" sizes="32x32">`,
+			`<link rel="icon" href="${wpAppConfig.siteIcon}" sizes="192x192">`, `<link rel="apple-touch-icon" href="${wpAppConfig.siteIcon}">`
+		);
+	}
+	return linkArray.join("");
+}
+/**
+ * 使用cdn加载依赖包
+ * @returns script标签字符串
+ */
+function loadExternalDependencies() {
+	if (process.env.NODE_ENV === "production") {
+		var prefix = "<script src='https://cdn.staticfile.org/",
+			suffix = ".global.prod.min.js'></script>";
+		return [
+			`${prefix}vue/3.2.25/vue.runtime${suffix}`,
+			`${prefix}vue-router/4.0.14/vue-router${suffix}`,
+			`${prefix}vuex/4.0.2/vuex${suffix}`
+		].join("");
+	} else return "";
+}
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [vue()],
+	plugins: [
+		vue(),
+		createHtmlPlugin(
+			{
+				inject: {
+					data: {
+						title: wpAppConfig.siteTitle,
+						injectMeta: generateSiteMeta(),
+						injectLink: generateSiteLink(),
+						injectScript: loadExternalDependencies(),
+					}
+				}
+			}
+		)],
 	build: {
 		rollupOptions: {
 			external: ["vue", "vuex", "vue-router"],
@@ -16,14 +71,13 @@ export default defineConfig({
 					"vue-router": "VueRouter",
 				}),
 			],
-			input:
-				process.env.NODE_ENV === "production" ? "index.production.html" : "index.html"
 		},
 	},
 	resolve: {
 		alias: {
 			"@": path.resolve("src"),
 			"@sty": path.resolve("src/style"),
+			"@wpAppConfig": path.resolve("./wp-app.config.js")
 		}
 	}
 })
---
+++ wp-app.config.js
@@ -0,0 +1,47 @@
+/**
+ * 站点及rest api配置信息
+ */
+export default {
+    /**
+     * 本站点标题
+     */
+    siteTitle: "小流星的梦",
+    /**
+     * 本站点描述，用于搜索引擎优化
+     */
+    siteDescription: "主题SPA版本开发测试",
+    /**
+     * 站点图标URL
+     */
+    siteIcon: "http://localhost:2222/wp-content/uploads/2022/03/cropped-6b6fec0efd8b0e996cb4f1ae7842571b-Mini.png",
+    /**
+     * 本站点URL，非API URL
+     */
+    siteUrl: "http://localhost:3000",
+    /**
+     * API基准URL
+     */
+    apiBaseUrl: "http://localhost:2222/wp-json/wp/v2/",
+    /**
+     * 访问wordpress rest api的用户名，
+     * 与应用程序密码同在wordpress个人资料里设置
+     */
+    apiUser: "debugger",
+    /**
+     * 应用程序密码，用来授权对wordpress rest api的访问
+     */
+    appPwd: "gxYX wxyy aSQJ tAEI 5NyB AarZ",
+    /**
+     * 链接结构，参考wordpress设置中“固定链接”
+     * 
+     * 默认为y-m-postname
+     * 
+     * 供选择的用来组合URL的子项：
+     * y:年，
+     * m:月，
+     * d:日，
+     * postname:文章名称，
+     * postid:文章ID
+     */
+    urlPattern: "y-m-postname"
+}
